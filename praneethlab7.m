da5b0849fa2482df33b089368af1b9e9933afb49
bt20ece043-praneeth <123888091+bt20ece043-praneeth@users.noreply.github.com>
2023-04-18 20:20:30 +0530

    Add files via upload

---
+++ praneethlab7.m
@@ -0,0 +1,31 @@
+% Lab Task - 7
+% BT20ECE043 K Venkat Praneeth
+%  20 Feb 2023
+
+word = 'ABBAS';
+code = arithmetic_coding(word)
+
+function code = arithmetic_coding(word)
+alphabet = ['A', 'B', 'S'];
+probability = [2/5, 2/5, 1/5];
+
+
+left = 0;
+right = 1;
+cumulative_prob = [0, cumsum(probability)];
+
+
+for i = 1:length(word)
+    
+    symbol_idx = find(alphabet == word(i));
+    symbol_left = left + (right - left) * cumulative_prob(symbol_idx);
+    symbol_right = left + (right - left) * cumulative_prob(symbol_idx+1);
+    
+    
+    left = symbol_left;
+    right = symbol_right;
+end
+
+
+code = (left + right) / 2;
+end
